1d707b429f84fa4f788d332a09a51b0d2441da96
Pablo Ochoa <pabloochoa29@hotmail.com>
2026-03-19 01:16:55 -0600

    ff

--- src/data/authors.js
+++ src/data/authors.js
@@ -4,7 +4,7 @@ const authors = {
   "p.ochoa@literatura.com": {
     name: "Pablo Ochoa Carpinteyro",
     image: pabloProfile,
-    bio: "músico y estudiante de literatura. Su trabajo se centra en la relación entre música y escritura, con interés en la forma, el ritmo y la construcción del sentido. Actualmente desarrolla textos narrativos y ensayísticos dentro de su formación académica."
+    bio: "Músico y estudiante de literatura. Me centro en la relación entre música y escritura, con interés en la forma, el ritmo y la construcción del sentido."
   },
 
   "j.lazcano@literatura.com": {
